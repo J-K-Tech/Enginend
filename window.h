4309e47de5251dd5b0992424102e6b3beb6ee257
J-K-Tech <82116913+J-K-Tech@users.noreply.github.com>
2021-12-27 12:11:51 +0000

    Update window.h

--- window.h
+++ window.h
@@ -106,8 +106,8 @@ void final()
 	int winy=(glutGet(GLUT_WINDOW_HEIGHT)-win_height)/2;
 	glViewport(winx,winy, win_width, win_height);
 //	printf("OpenGL version supported by this platform (%s): \n", glGetString(GL_VERSION));
-    //glClearColor(0.,0.,0.,0.);
-    //glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glClearColor(0.,0.,0.,0.);
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, color);
